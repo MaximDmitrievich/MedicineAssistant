3846e902e5d18430e02a57c1e37f1ce258891037
Максим <mderemin@gmail.com>
2018-01-31 15:53:24 +0300

    Delete Temperature.ino

--- IoT-Module/App/Temperature.ino
+++
@@ -1,9 +0,0 @@
-float getTemperature() {
-  float temp;
-  do {
-    DS18B20.requestTemperatures(); 
-    temp = DS18B20.getTempCByIndex(0);
-    delay(100);
-  } while (temp == 85.0 || temp == (-127.0));
-  return temp;
-}
